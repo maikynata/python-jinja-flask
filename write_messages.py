d10a76bb38c65ead8ad832e77d99909e7ad361ce
maikynata <maikynata25@gmail.com>
2024-06-30 13:40:48 +0200

    using if statements

--- write_messages.py
+++ write_messages.py
@@ -8,6 +8,8 @@ students = [
     {"name": "Sandrine",  "score": 100},
     {"name": "Gergeley", "score": 87},
     {"name": "Frieda", "score": 92},
+    {"name": "Fritz", "score": 40},
+    {"name": "Sirius", "score": 75},
 ]
 
 environment = Environment(loader=FileSystemLoader("templates/"))
